9984603c7a2479841b8893dacad0d8d51fc33720
Derek Jones <mysyzygy@gmail.com>
2017-05-12 22:32:51 -0700

    Adding example .txt and .npy files from potc.wav analysis

--- spectral_loudness.py
+++ spectral_loudness.py
@@ -7,9 +7,9 @@ def main():
     spectral_loudness.main() is the main command line entry point to use the spectral_loudness package.
     """
     # parse command line arguments
-    parser = argparse.ArgumentParser(description='Analyze spectral loudness of a 16-bit wav file.')
-    parser.add_argument('--input_file', '-i', help='Path to input file')
-    parser.add_argument('--output_file', '-o', help='Path to output file')
+    parser = argparse.ArgumentParser(description='Analyze spectral loudness of a 48 kHz, 16-bit stereo wav file.')
+    parser.add_argument('--input_file', '-i', help='Path to input file. Must be .wav')
+    parser.add_argument('--output_file', '-o', help='Path to output file. Must be .txt or .npy')
     args = parser.parse_args()
 
     # create engine object
